d9ae9c18ebd752f297a26a0b4b4a13a2619ed6a7
AtanuPanja <74297632+AtanuPanja@users.noreply.github.com>
2020-11-14 19:19:08 +0530

    Add files via upload

---
+++ Fibonacci Iterative .c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(int argc, char *argv[])
+{
+	int pos;
+	printf("The Fibonacci Series looks like this\n0 1 1 2 3 5 8 13 . . .\nEnter the position of the Fibonnacci term you want\n");
+	scanf("%d",&pos);
+	int a = 0, b = 1,c;
+	for(int i = 1; i < pos-1; i++)
+	{
+		b = a + b;
+		a = b - a;
+	}
+	if(pos > 0)
+	printf("The Fibonacci Term at position %d is %d\n",pos,b);
+	else
+	printf("Please enter value greater than 0\n");
+	return 0;
+}
