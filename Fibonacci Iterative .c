c7fdf4b4289f14e2611a6bcc7efdb8e54c509b83
AtanuPanja <74297632+AtanuPanja@users.noreply.github.com>
2020-11-14 19:36:44 +0530

    Update Fibonacci Iterative .c

--- Fibonacci Iterative .c
+++ Fibonacci Iterative .c
@@ -4,7 +4,7 @@ int main(int argc, char *argv[])
 	int pos;
 	printf("The Fibonacci Series looks like this\n0 1 1 2 3 5 8 13 . . .\nEnter the position of the Fibonnacci term you want\n");
 	scanf("%d",&pos);
-	int a = 0, b = 1,c;
+	int a = 0, b = 1;
 	for(int i = 1; i < pos-1; i++)
 	{
 		b = a + b;
